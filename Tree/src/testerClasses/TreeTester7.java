6bfe22c35c4c7d28a94d89966f257e33b8ea34a9
Cesar <cesar.justiniano@upr.edu>
2018-04-23 20:32:14 -0400

    Completed Lab

--- Tree/src/testerClasses/TreeTester7.java
+++ Tree/src/testerClasses/TreeTester7.java
@@ -1,18 +1,20 @@
 package testerClasses;
 
+import labUtils.Utils;
+import treeClasses.LinkedBinaryTree;
 import treeClasses.LinkedTree;
 
 public class TreeTester7 {
 
-	public static void main(String[] args) {
-		LinkedTree<String> t = new LinkedTree<>(); 
+	public static void main(String[] args) throws CloneNotSupportedException {
+		LinkedTree<Integer> t = Utils.buildExampleTreeAsLinkedTree(); 
+		LinkedTree<Integer> tClone = t.clone();
 		
-		try {
-			LinkedTree<String> tClone = t.clone();
-		} catch (CloneNotSupportedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+		// display content as a tree
+		Utils.displayTree("The original linked tree is: ", t); 
+				
+		// display content as a tree
+		Utils.displayTree("The clone linked tree is: ", tClone); 
 	}
 
 }
